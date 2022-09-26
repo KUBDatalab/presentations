4d85f0a67be1338fb5d9a2758abd45ea9995e6b0
Christian Knudsen <christian.knudsen@gmail.com>
2022-09-26 16:13:24 +0200

    daf

--- render.R
+++
@@ -1,8 +0,0 @@
-library(stringr)
-liste <- list.files(path = "slides", pattern = ".Rmd", recursive = T, include.dirs = T, full.names = T)
-
-
-for(i in 1:length(liste)){
-  rmarkdown::render(liste[i])
-}
-
