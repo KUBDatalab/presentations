bd5d35374d93688c2599e9823afbc0cafa8557aa
Christian Knudsen <christian.knudsen@gmail.com>
2022-09-27 11:35:48 +0200

    output bør defineres i yaml

--- render.R
+++ render.R
@@ -4,5 +4,8 @@ files <- list.files(path = "slides", pattern = "\\.?md", recursive = T)
 files <- here(here(), "slides", files)
 
 for(i in 1:length(files)){
-  rmarkdown::render(files[i], output_format = "slidy_presentation")
+  rmarkdown::render(files[i])
 }
+
+# bør det ikke være defineret i yaml?
+#, output_format = "slidy_presentation"
